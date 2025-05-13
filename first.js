cd0974d04dce2f70283506bcfa1eae75e222af50
shoaib ahmad <shoaibahmad@shoaibs-MacBook-Air.local>
2025-05-13 16:42:03 +0530

    addcode

---
+++ first.js
@@ -0,0 +1,19 @@
+const product = {
+  name: "parker pen",
+  rating: 4,
+  offer: 5,
+  price: 270,
+};
+console.log(product);
+
+
+const profile = {
+  user_name:'zaryab',
+  followers:10000,
+  following:1,
+  discription:'in all things be men',
+  posts:129,
+  is_follow:true
+};
+
+console.log(profile);
