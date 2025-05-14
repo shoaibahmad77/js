1dab5a5d8f13073a9b0721d8e049688c9bfea65d
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-14 15:23:48 +0530

    made changes

---
+++ 1.js
@@ -0,0 +1,35 @@
+// js is dynamically typed we need not to tell what we are storing in a varible
+
+// To print
+console.log("hello world");
+
+// variables:- these are the containers used to store values
+// number
+age = 25;
+console.log(age);
+// string
+name = "areeb";
+console.log("name");
+// boolean
+isFollow = true;
+console.log(isFollow);
+/* null:- absense of any value */
+x = null;
+console.log(x);
+/* undefined:- means a variable has been declared but not assigned a value yet */
+y = undefined;
+console.log(y);
+/*
+          variables rules
+1. variables names are case sensitive "a" and "A" are different
+2. only letters,digits,underscore(_) and $ is allowed,
+  below eg will show error
+
+1name = "shoaib"
+console.log(1name); 
+3. only letters,underscore(_) and $ can be 1st character
+
+4. reserved words cannot be variables names eg console = 25;
+*/
+
+
