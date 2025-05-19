e9aa30d531b4632e3b670a8ee70f669c05c46b7d
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-19 11:37:57 +0530

    notes

--- 01_variables.js
+++ 01_variables.js
@@ -1,7 +1,40 @@
+// JS is a dynamically typed langauge we need not to tell the type of data we are storing in a variable.
+
 // variables are containers used to store data
+
+// JavaScript Data Types:
+
+// Primitive Types:
+// Number: Integer or floating-point (e.g., 42, 3.14).
+// String: Text data (e.g., "hello").
+// Boolean: true or false.
+// Undefined: Variable declared but not assigned (e.g., let x;).
+// Null: Explicitly no value (e.g., let x = null;).
+// Symbol: Unique, immutable identifier (e.g., Symbol("id")).
+// BigInt: Large integers beyond Number limits (e.g., 123n).
+
+// Non-Primitive (Reference) Types:
+// Object: Key-value pairs (e.g., { name: "John" }).
+// Array: Ordered list (e.g., [1, 2, 3]).
+// Function: Reusable code block (e.g., function add() {}).
+
 name = "tony stark";
-console.log(name)
+console.log(name);
 
 age = 25;
 console.log(age);
-console.log('enad')
+
+// null :- absense of value its a special value
+
+x = null;
+console.log(x);
+
+// undefined :- dont know whats inn
+
+y = undefined;
+console.log(y);
+
+isFollow = true;
+console.log(isFollow);
+
+ 
