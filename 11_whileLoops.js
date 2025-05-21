4a633c93ac5bdba13d26357163ebe7ee0572efc3
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-21 20:31:28 +0530

    do-while loop

---
+++ 11_whileLoops.js
@@ -0,0 +1,36 @@
+// while:Explanation of While Loops in JavaScript (Easy)
+// A while loop in JavaScript is a way to repeat a block of code as long as a condition is true. It’s useful when you don’t know exactly how many times you need to loop, but you want to keep going until a specific condition changes.
+
+// Write a JavaScript program using a while loop to print numbers from 1 to 10.
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+// Write a JavaScript program using a while loop to print "Hello" 5 times.
+
+// let string = 1;
+// while (string <= 5) {
+//   console.log("hello");
+//   string++;
+// }
+// do while
+// let i = 20;
+// do {
+//   console.log("zari ke gand");
+//   i++;
+// } while (i <= 10);
+
+// Write a JavaScript program using a do...while loop to print numbers from 1 to 10.
+// let i=1;
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i<=10);
+let j = 1;
+do {
+  console.log(j);
+  j += 2;
+} 
+while (j <= 9);
