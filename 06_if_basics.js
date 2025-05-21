ea8087acdafbf372cc322f562c48b78cd37557c4
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-21 13:26:33 +0530

    made some shit

---
+++ 06_if_basics.js
@@ -0,0 +1,61 @@
+// conditional statements
+// The if statement in JavaScript is used to execute a block of code if a condition is true. It can be extended with else or else if to handle alternative conditions
+
+// syntax
+/* 
+if (condition) {
+   // Code runs if condition is true
+ } else if (anotherCondition) {
+   // Code runs if anotherCondition is true
+ } else {
+   // Code runs if no conditions are true
+} 
+   */
+// Write a JavaScript program to check if a number is greater than 10. If true, print "Greater than 10."
+
+// let number = prompt('enter your number');
+// if(number >10){
+//   console.log("greater than 10",number)
+// }
+// else {
+//   console.log('not greater than 10')
+// }
+
+// Write a JavaScript program to check if a number is negative. If true, print "Negative," otherwise print "Non-negative."
+// let no = prompt("enter number");
+// if (no < 0) {
+//   console.log("its negative");
+// } else {
+//   console.log("its positive");
+// }
+
+// Write a JavaScript program to check if a person's age is 18 or older. If true, print "Adult," else print "Minor."
+// let person_age = 22;
+// if (person_age>=18){
+//   console.log('adult')
+// }
+// else{
+//   console.log('minor')
+// };
+//  Write a JavaScript program to check if a string is "hello". If true, print "Hello detected," else print "Not hello."
+
+// let string_ = prompt('enter string:')
+// if(string_==='hello'){
+//   console.log('hello detected')
+// }
+// else{
+//   console.log('not hello')
+// }
+
+
+
+// Write a JavaScript program to check a person's score: if 80 or above, print "High"; if 50 to 79, print "Medium"; else print "Low."
+
+// let score = 2;
+// if (score > 80) {
+//   console.log("high", score);
+// } else if (score > 50 && score <= 79) {
+//   console.log("medium", score);
+// } else {
+//   console.log("low");
+// }
