eb12647d86dede5d7e21f126595c0a00ac0e697e
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-21 19:51:43 +0530

    infinite loop

---
+++ 10_forLoops.js
@@ -0,0 +1,39 @@
+// A for loop in JavaScript is a way to repeat a block of code a specific number of times.
+// Why Use a For Loop?
+// To repeat code a fixed number of times (e.g., count from 1 to 10).
+// To go through items in an array (e.g., print each fruit in a list).
+// To automate repetitive tasks in a clear way
+
+//  for (initialization; condition; update) {
+// Code to run each loop }
+// let i = 0;
+// for (i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+// will output 0,1,2,3,4,5
+// print 5x apna college
+// let x  = 'apna college'
+// for (x=1;x<=5;x++){
+//     console.log('apna college')
+// }
+// Write a JavaScript program using a for loop to print numbers from 1 to 10.
+// let num = 1;
+// for(num=1;num<=10;num++){
+//     console.log(num);
+// }
+// Write a JavaScript program using a for loop to print even numbers from 2 to 20.
+// let even_number=20
+// for(even_number=2;even_number<=20;even_number+=2){
+//     console.log(even_number);
+// }
+
+// Write a JavaScript program using a for loop to count down from 10 to 1.
+// let count = 10;
+// for(count=10;count>=1;count--){
+//     console.log(count)
+// }
+// infinite loop 
+// let i;
+// for(let i=1;i>=0;i++){
+//     console.log(i);
+// }
