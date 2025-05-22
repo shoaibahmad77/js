d246c05480885fd08b23b98941dbb9d907eab257
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-22 12:30:03 +0530

    even-odd

---
+++ loop-practice-ques.js
@@ -0,0 +1,15 @@
+// lets practice
+// qs1: print all even numbers from 0 to 100
+
+// let i=0;
+// for(i=0;i<=100;i++){
+//     if(i%2==0){
+//         console.log('even numbers are:',i)
+//     }
+
+// }
+//  print all odd numbers from 0-100
+let j = 1;
+for (j = 1; j <= 100; j += 2) {
+  console.log(j);
+}
