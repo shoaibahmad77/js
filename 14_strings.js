a8db2bece52bd8bc14bc87ba08f1accd801b471d
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-22 15:43:23 +0530

    template literals - backtick

---
+++ 14_strings.js
@@ -0,0 +1,39 @@
+// strings: are seq of char
+// how to create
+// let str = "string name";
+// inbuilt prop and fuction (methods)
+// properties :-
+//  to print string lenght we can use  stringname.length
+// to access index  string[0]
+
+// let str = "waseem";
+// console.log(str.length);
+// console.log(str[0]);
+/* template literals  in JS 
+a way to have embedded expersions in strings 
+
+
+
+*/
+// template literals
+// let sentence =`this is a template literals `  // using backticks
+
+// let details ={
+//     product_name:'pen',
+//     product_price:45
+// }
+// details.product_price=5;
+// details.product_name='book'
+// console.log(`the product is a ${details.product_name} and it has a price of ${details.product_price}`)
+
+let student = {
+  name: "nabeeha",
+  class: 7,
+  school: "mallinson",
+};
+
+student.name = "areeb";
+student.school = "biscoe";
+console.log(
+  `the name of the student is ${student.name} and he reads in class ${student.class} and studies in ${student.school} school`
+);
--- loop-practice-ques.js
+++ loop-practice-ques.js
@@ -9,7 +9,17 @@
 
 // }
 //  print all odd numbers from 0-100
-let j = 1;
-for (j = 1; j <= 100; j += 2) {
-  console.log(j);
+// let j = 1;
+// for (j = 1; j <= 100; j += 2) {
+//   console.log(j);
+// }
+// create a game where you start with any random number.aks the user to keep guessing the game number until the user enters correct value
+
+let game_number = 27;
+let user_number = prompt("enter your guess");
+while (user_number != game_number) {
+  user_number = prompt("try again");
 }
+console.log("you made the correct guess");
+
+//prompt returns string
