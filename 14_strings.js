fdbdb81d2c74f4a1a324b540111636747c55255e
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-22 16:07:13 +0530

    trim() in strings

--- 14_strings.js
+++ 14_strings.js
@@ -26,17 +26,40 @@ a way to have embedded expersions in strings
 // details.product_name='book'
 // console.log(`the product is a ${details.product_name} and it has a price of ${details.product_price}`)
 
-let student = {
-  name: "nabeeha",
-  class: 7,
-  school: "mallinson",
-};
+// let student = {
+//   name: "nabeeha",
+//   class: 7,
+//   school: "mallinson",
+// };
 
-student.name = "areeb";
-student.school = "biscoe";
-console.log(
-  `the name of the student is ${student.name} and he reads in class ${student.class} and studies in ${student.school} school`
-);
+// student.name = "areeb";
+// student.school = "biscoe";
+// console.log(
+//   `the name of the student is ${student.name} and he reads in class ${student.class} and studies in ${student.school} school`
+// );
 //this technique is called string interpolation
-// escape character :- used fro string next line
-console.log("apna \n college")
+// escape character :- used fro string next line /n
+// console.log("apna \n college")
+// tab space /t
+// let word ='hello\tworld'  //12
+// console.log(word.length) 
+// \t counts as 1 length
+
+// string methods :- methods to manipulate a string
+/* 
+str.toUpperCase()
+str.toLowerCase()
+str.trim() // remove whitespaces
+*/
+
+// console.log(word.toLocaleUpperCase())
+// console.log(word.toLowerCase())
+// let str ="hello"
+// let new_str =str.toUpperCase();
+// console.log(str)
+// console.log(new_str)
+
+// strings are immutable  we cant change 
+// trim :- starting and ending ke gaps khtm krta hai
+let str ="     hello  apna   college   "
+console.log(str.trim())
