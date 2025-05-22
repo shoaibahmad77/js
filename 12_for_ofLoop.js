7bfe12dcfab665e2f55272f70c8b109740041aed
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-22 12:05:13 +0530

    for_of

--- 12_for_ofLoop.js
+++ 12_for_ofLoop.js
@@ -1,4 +1,23 @@
 // for of loop
 
 //they help to apply loop on some special data types like arrays ,string
-// syntax  for(let val of strVar){// do some work}
+// syntax  for(let val of strVar){// do some work} 
+// no initialization , updation, no stopping condition needed 
+
+// for-of loop
+let str ='apnaCollege'
+for(let i  of str){
+    console.log('i:',i)
+}
+
+// it print each character in the string
+// Ques; calculate no of values in 'world'
+
+let size =0;
+let str_ ='world'
+for(let i of str_){
+    console.log( "string:",i)
+    size++;
+}
+
+console.log(size);
