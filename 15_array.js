e4128c30887d5b2e2a70bb63624336305afc4712
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-23 17:15:13 +0530

    arrays

--- 15_array.js
+++ 15_array.js
@@ -2,11 +2,19 @@
 // good to store related data -linear form
 
 // example how to store , we can store diff types of data
-let arr = ["hulk", "spiderman", "superman", 92];
-console.log(arr);
+// let arr = ["hulk", "spiderman", "superman", 92];
+// console.log(arr);
 
 //  arrays indicies arr[0] ...
-console.log(arr[5]);
-// to change value -> arr[0]='skaitiman
-arr[0] = "shaktiman";
-console.log(arr);
+// console.log(arr[5]);
+// // to change value -> arr[0]='skaitiman
+// arr[0] = "shaktiman";
+// console.log(arr);
+//  arrays are mutable
+// loops and arrays
+// prop length
+
+let marks = [12, 13, 14, 15, 16,17,18];
+for (i = 0; i < marks.length; i++) {
+  console.log(marks[i]);
+}
