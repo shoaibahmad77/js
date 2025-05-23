7b4a59bffcc16e0ecd9109fa08ff11c8992aee78
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-23 16:11:45 +0530

    arrays

--- 15_array.js
+++ 15_array.js
@@ -1,2 +1,12 @@
 //  array : theses are collection of items
+// good to store related data -linear form
 
+// example how to store , we can store diff types of data
+let arr = ["hulk", "spiderman", "superman", 92];
+console.log(arr);
+
+//  arrays indicies arr[0] ...
+console.log(arr[5]);
+// to change value -> arr[0]='skaitiman
+arr[0] = "shaktiman";
+console.log(arr);
