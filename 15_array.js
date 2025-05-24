40d16839f1d74370b12f657cffeb15e2f1b4772b
shoaibahmad77 <ahmadshoaib4444@gmail.com>
2025-05-24 17:21:11 +0530

    practice ques on splice

--- 15_array.js
+++ 15_array.js
@@ -80,11 +80,33 @@
 
 // splice is used to add remove delete in an array syntax arr.splice(startINDEX,endINDEX,element to be added )
 
-let bros =['zaryab','furkan','fahad','ahmer','saif']
+// let bros =['zaryab','furkan','fahad','ahmer','saif']
 
-bros.splice(1,2,'sharana')
-console.log(bros)
+// bros.splice(1,2,'sharana')
+// console.log(bros)
 
-let arr =[1,2,3,4,5,6,7]
-arr.splice(1,0,9)
-console.log(arr)
+// add element
+// let arr =[1,2,3,4,5,6,7]
+// arr.splice(1,0,9)
+// console.log(arr)
+// // delete element
+// arr.splice(0,1)
+// console.log(arr)
+// replace item lets replace 9 by 10
+
+// arr.splice(0,1,10)
+
+// console.log(arr)
+
+//lets practice
+// ques; create an array to store companies , bloomberg, microsoft,uber ,google,ibm,netflix
+// a. remove the first compnay from the array, b. remove uber and add ola, c. add amazon at the end.
+
+let companies=[ 'bloomberg', 'microsoft','uber','google','ibm','netflix']
+
+companies.splice(0,1)
+console.log(companies)
+companies.splice(1,1,'ola')
+console.log(companies)
+companies.splice(6,0,'amazon')
+console.log(companies)
